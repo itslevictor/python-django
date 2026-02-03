659e4b9ac51788eeb4e39a4015df4c9a13375c06
victor <itslevictor@gmail.com>
2026-02-03 17:48:22 -0300

    v0.1 - primeira atualizacao

---
+++ setup/settings.py
@@ -0,0 +1,22 @@
+import os # no topo do arquivo
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME', 'postgres'),
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': 'db', 
+        'PORT': '5432',
+    }
+}
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'core',
+]
